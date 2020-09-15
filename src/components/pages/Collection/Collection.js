7624bb68002a14733ca577c3e769e65408626d1c
Michael Dotson <thedotson@icloud.com>
2020-09-14 21:08:01 -0500

    added base components to prep for routing

---
+++ src/components/pages/Collection/Collection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Collection.scss';
+
+class Collection extends React.Component {
+  render() {
+    return (
+      <div className="collection">
+        <h1>My Collection</h1>
+      </div>
+    );
+  }
+}
+
+export default Collection;
