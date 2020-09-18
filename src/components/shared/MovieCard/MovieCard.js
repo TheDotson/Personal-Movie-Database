34dec4a190b909bdc6ae0d2542fe1e99c10c2331
Michael Dotson <thedotson@icloud.com>
2020-09-17 20:33:00 -0500

    add movie to collection works

--- src/components/shared/MovieCard/MovieCard.js
+++ src/components/shared/MovieCard/MovieCard.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import collectionData from '../../../helpers/data/collectionData';
+import authData from '../../../helpers/data/authData';
 import './MovieCard.scss';
 
 class MovieCard extends React.Component {
+
+  addToCollection = (e) => {
+    e.preventDefault();
+    const { movie } = this.props;
+    const newMovie = {
+      overview: movie.overview,
+      poster_path: movie.poster_path,
+      title: movie.title,
+      uid: authData.getUid(),
+      watched: false,
+    };
+    collectionData.addMovie(newMovie)
+      .then()
+      .catch((err) => console.error('Add movies failed', err));
+  }
+
   render() {
     const { movie } = this.props;
     const moviePoster = `https://image.tmdb.org/t/p/w342${movie.poster_path}`;
@@ -15,7 +33,7 @@ class MovieCard extends React.Component {
           <p className="text-center">{movie.overview}</p>
         </div>
         <div className="card-footer">
-          <i className="fas fa-eye"></i>
+          <i className="fas fa-eye" onClick={this.addToCollection}></i>
           <i className="fas fa-plus-circle"></i>
         </div>
       </div>
