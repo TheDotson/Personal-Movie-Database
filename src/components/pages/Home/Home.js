98a0c80a58a6bf9278895dd6eade175f1fec1fa2
Michael Dotson <thedotson@icloud.com>
2020-09-21 18:51:24 -0500

    added brief description for home page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -6,6 +6,9 @@ class Home extends React.Component {
     return (
       <div className="home">
         <h1>Personal Movie Database</h1>
+        <p>Have you ever caught yourself trying to remember whether or not you own a movie?  Or better yet, what format you own the movie?<br/>
+        That's why the PMDB was made!<br/>
+        This simple app will allow for even the most extensive of collections to be catalogued.</p>
       </div>
     );
   }
